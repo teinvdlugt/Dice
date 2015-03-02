789297775f971ce37840418c9c3b4de51664e82f
Tein van der Lugt <teinvdlugt@gmail.com>
2015-03-02 20:29:24 +0100

    Add "Go" and "Dance" buttons

--- app/src/main/java/com/teinproductions/tein/dice/MainActivity.java
+++ app/src/main/java/com/teinproductions/tein/dice/MainActivity.java
@@ -1,22 +1,30 @@
 package com.teinproductions.tein.dice;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
 
-    DieView mDieView;
+    private DieView dieView1, dieView2;
+    private Button danceButton;
+
+    private boolean dancing = false;
+    private Dancer dancer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mDieView = (DieView) findViewById(R.id.die_view);
+        dieView1 = (DieView) findViewById(R.id.die_view1);
+        dieView2 = (DieView) findViewById(R.id.die_view2);
+        danceButton = (Button) findViewById(R.id.dance_button);
     }
 
 
@@ -42,27 +50,48 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void onClickOne(View view) {
-        mDieView.setPips(DieView.ONE);
+    public void onClickGo(View view) {
+        final int pips1 = (int) Math.floor(Math.random() * 6) + 1;
+        final int pips2 = (int) Math.floor(Math.random() * 6) + 1;
+        dieView1.setPips(pips1);
+        dieView2.setPips(pips2);
     }
 
-    public void onClickTwo(View view) {
-        mDieView.setPips(DieView.TWO);
+    public void onClickDance(View view) {
+        if (dancing) {
+            dancer.cancel(true);
+            danceButton.setText(R.string.dance);
+            dancing = false;
+        } else {
+            dancer = new Dancer();
+            dancer.execute();
+            dancing = true;
+            danceButton.setText(R.string.stop_dancing);
+        }
     }
 
-    public void onClickThree(View view) {
-        mDieView.setPips(DieView.THREE);
-    }
+    class Dancer extends AsyncTask<Void, Void, Void> {
 
-    public void onClickFour(View view) {
-        mDieView.setPips(DieView.FOUR);
-    }
+        @Override
+        protected Void doInBackground(Void... params) {
 
-    public void onClickFive(View view) {
-        mDieView.setPips(DieView.FIVE);
-    }
+            //noinspection ConstantConditions
+            while (1 + 1 == 2) {
+                if (isCancelled()) {
+                    return null;
+                }
+                publishProgress();
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
-    public void onClickSix(View view) {
-        mDieView.setPips(DieView.SIX);
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            onClickGo(null);
+        }
     }
 }
